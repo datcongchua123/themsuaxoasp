88007c3ab69b757c599e7b63160dd1879e0d7430
datcongchua123 <hoaang2005@gmail.com>
2023-11-30 13:42:45 +0700

    Add project files.

---
+++ baikiemtra1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace baikiemtra1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btntimkiem = new System.Windows.Forms.Button();
+            this.txttimkiem = new System.Windows.Forms.TextBox();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.txtsoluong = new System.Windows.Forms.TextBox();
+            this.txtgia = new System.Windows.Forms.TextBox();
+            this.txttensp = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên SP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Giá SP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(480, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số lượng tồn";
+            // 
+            // btntimkiem
+            // 
+            this.btntimkiem.Location = new System.Drawing.Point(643, 42);
+            this.btntimkiem.Name = "btntimkiem";
+            this.btntimkiem.Size = new System.Drawing.Size(141, 27);
+            this.btntimkiem.TabIndex = 4;
+            this.btntimkiem.Text = "Tìm Kiếm?";
+            this.btntimkiem.UseVisualStyleBackColor = true;
+            this.btntimkiem.Click += new System.EventHandler(this.btntimkiem_Click);
+            // 
+            // txttimkiem
+            // 
+            this.txttimkiem.BackColor = System.Drawing.Color.MistyRose;
+            this.txttimkiem.Location = new System.Drawing.Point(173, 28);
+            this.txttimkiem.Multiline = true;
+            this.txttimkiem.Name = "txttimkiem";
+            this.txttimkiem.Size = new System.Drawing.Size(446, 41);
+            this.txttimkiem.TabIndex = 5;
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(109, 435);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 23);
+            this.btnthem.TabIndex = 6;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(390, 435);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 23);
+            this.btnsua.TabIndex = 7;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(643, 435);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 23);
+            this.btnxoa.TabIndex = 8;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // txtsoluong
+            // 
+            this.txtsoluong.Location = new System.Drawing.Point(582, 146);
+            this.txtsoluong.Name = "txtsoluong";
+            this.txtsoluong.Size = new System.Drawing.Size(220, 22);
+            this.txtsoluong.TabIndex = 9;
+            // 
+            // txtgia
+            // 
+            this.txtgia.Location = new System.Drawing.Point(301, 221);
+            this.txtgia.Name = "txtgia";
+            this.txtgia.Size = new System.Drawing.Size(225, 22);
+            this.txtgia.TabIndex = 10;
+            // 
+            // txttensp
+            // 
+            this.txttensp.Location = new System.Drawing.Point(109, 149);
+            this.txttensp.Name = "txttensp";
+            this.txttensp.Size = new System.Drawing.Size(225, 22);
+            this.txttensp.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Info;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(66, 276);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(736, 138);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(889, 483);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txttensp);
+            this.Controls.Add(this.txtgia);
+            this.Controls.Add(this.txtsoluong);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.txttimkiem);
+            this.Controls.Add(this.btntimkiem);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Đạt Đẹp Trai";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btntimkiem;
+        private System.Windows.Forms.TextBox txttimkiem;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.TextBox txtsoluong;
+        private System.Windows.Forms.TextBox txtgia;
+        private System.Windows.Forms.TextBox txttensp;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ baikiemtra1/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace baikiemtra1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con;
+        SqlCommand cmd;
+        private void concection()
+        {
+            String strcon = "server=LAPTOP-CREE9M5V\\SQLEXPRESS;database=QLSP;integrated security=true";
+            con = new SqlConnection(strcon);
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loaddata();
+        }
+        private void cleardata()
+        {
+            txttensp.Clear();
+            txtgia.Clear();
+            txtsoluong.Clear();
+            txttimkiem.Clear();
+        }
+        private void loaddata()
+        {
+            concection();
+            SqlDataAdapter da = new SqlDataAdapter("select * from QLSP", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+
+        }
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            concection();
+            cmd = new SqlCommand("update QLSP set tensp='" + txttensp.Text + "',gia='" + Convert.ToDouble(txtgia.Text) + "',soluongton='" + Convert.ToInt16(txtsoluong.Text) + "'where tensp='" + txttensp.Text + "'", con);
+            con.Open();
+            int ret = cmd.ExecuteNonQuery();
+            if (ret == 1)
+                MessageBox.Show("Cap nhat xong roi may!");
+            con.Close();
+            loaddata();
+            cleardata();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txttensp.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtgia.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtsoluong.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+
+        }
+        private void btntimkiem_Click(object sender, EventArgs e)
+        {
+            concection();
+            SqlDataAdapter da = new SqlDataAdapter("select * from QLSP where tensp LIKE '%" + txttimkiem.Text + "%' and soluongton <100 and gia > 1000", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+            cleardata();
+            
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            concection();
+            cmd = new SqlCommand("delete from QLSP where tensp= '" + txttensp.Text + "'", con);
+            con.Open();
+            int ret = cmd.ExecuteNonQuery();
+            if (ret == 1)
+                MessageBox.Show("Khong thich xoa day^^");
+            con.Close();
+            loaddata();
+
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            concection();
+            cmd = new SqlCommand("insert into qlsp values('" + txttensp.Text + "','" + Convert.ToInt16(txtgia.Text) + "','" + Convert.ToInt16(txtsoluong.Text) + "')", con);
+            con.Open();
+            int ret = cmd.ExecuteNonQuery();
+            if (ret == 1)
+            {
+                MessageBox.Show("Them Thanh Cong!");
+            }
+            con.Close();
+            loaddata();
+            cleardata() ;
+        }
+    }
+}
